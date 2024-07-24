98db76f42c10f10364374efddf52640c339cc6a5
김인엽 <109032883+Afdddd@users.noreply.github.com>
2024-07-24 19:16:41 +0900

    feat : 프로젝트 생성 및 세팅
    #프로젝트 생성
     Gradle : 8.8
     Spring Boot : 3.3.2
     Java : 17
     Dependencies : Mysql JPA SpringWeb Lombok
    
    #DB 연결

---
+++ src/main/java/com/flab/CommerceCore/CommerceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.flab.CommerceCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceCoreApplication.class, args);
+	}
+
+}
