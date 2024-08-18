dfd7476b8ba4072505afe3dd475faeee8134f31a
김인엽 <109032883+Afdddd@users.noreply.github.com>
2024-08-19 01:00:29 +0900

    refactor : 재고 동기성 처리 테스트 개선

--- src/test/java/com/flab/CommerceCore/inventory/domain/entity/InventoryTest.java
+++ src/test/java/com/flab/CommerceCore/inventory/domain/entity/InventoryTest.java
@@ -1,11 +1,15 @@
 package com.flab.CommerceCore.inventory.domain.entity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.flab.CommerceCore.common.exceptions.BusinessException;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,44 +27,41 @@ class InventoryTest {
 
   @Test
   void testReduceQuantityConcurrency() throws InterruptedException {
+    // given
+    int reduceCount = 2;
+    int threadCount = 100;
 
-    int reduceAmount = 10;
-    int threadCount = 10;
+    CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+    ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
 
-    List<Thread> threads = new ArrayList<>();
+    AtomicInteger success = new AtomicInteger();
+    AtomicInteger failure = new AtomicInteger();
 
-    // threadCount만큼 스레드를 생성하여 리스트에 추가
+    // when
     for (int i = 0; i < threadCount; i++) {
-      Thread thread = new Thread(() -> {
-        try {
-          Thread.sleep(50);  // 다른 스레드가 이 시점에 개입할 수 있도록 약간의 지연을 줌
-          inventory.reduceQuantity(reduceAmount);
-        } catch (InterruptedException e) {
-          Thread.currentThread().interrupt();
+      executorService.submit(() -> {
+        try{
+          inventory.reduceQuantity(reduceCount);
+          success.incrementAndGet();
+        }catch (BusinessException e){
+          failure.incrementAndGet();
+        }finally {
+          countDownLatch.countDown();
         }
       });
-      threads.add(thread);
     }
 
-    // 모든 스레드를 시작
-    for (Thread thread : threads) {
-      thread.start();
-    }
+    countDownLatch.await();
+    executorService.shutdown();
 
-    // 모든 스레드가 완료될 때까지 기다림
-    for (Thread thread : threads) {
-      thread.join();
-    }
-
-    // 최종 수량이 예상대로인지 확인
-    assertEquals(0, inventory.getQuantity(), "최종 수량이 예상과 다릅니다.");
-
+    // then
+    assertAll(
+        ()->assertThat(success.get()).isEqualTo(50),
+        ()->assertThat(failure.get()).isEqualTo(50)
+    );
   }
 
-  @Test
-  void testQuantityException() {
-    int reduceAmount = 101;
-    assertThrows(BusinessException.class,()->inventory.reduceQuantity(reduceAmount));
-  }
+
+
 
 }
