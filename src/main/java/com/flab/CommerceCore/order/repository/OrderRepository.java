a1bf091b492f7ba906b54c01026b63f7e9e38050
김인엽 <109032883+Afdddd@users.noreply.github.com>
2024-09-21 16:24:52 +0900

    feat: OrderRepository에 사용자 이메일 및 주문 ID로 조회하는 메서드 추가

--- src/main/java/com/flab/CommerceCore/order/repository/OrderRepository.java
+++ src/main/java/com/flab/CommerceCore/order/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package com.flab.CommerceCore.order.repository;
 
 import com.flab.CommerceCore.common.annotation.LogRepositoryError;
 import com.flab.CommerceCore.order.domain.entity.Order;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @LogRepositoryError
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+  List<Order> findAllByUserEmail(String userEmail);
+  Order findByOrderId(Long orderId);
 }
